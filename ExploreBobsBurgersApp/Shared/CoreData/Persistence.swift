a8c8bf51fedffa18b3f9e6bf6f24bb3451062bf3
ESmith <info@scriffles.com>
2023-08-11 10:15:49 -0500

    change cd persistence to not store in memory

--- ExploreBobsBurgersApp/Shared/CoreData/Persistence.swift
+++ ExploreBobsBurgersApp/Shared/CoreData/Persistence.swift
@@ -30,7 +30,7 @@ struct PersistenceController {
 
     let container: NSPersistentContainer
 
-    init(inMemory: Bool = true) {
+    init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "ExploreBobsBurgers")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
